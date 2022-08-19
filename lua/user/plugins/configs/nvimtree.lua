8b6b2dbdfbe05d58f81c1ebaebdca2d7895e850f
o.krasovskyi <mechanosec29@gmail.com>
2022-08-19 14:04:02 +0300

    nvim tree

--- lua/user/plugins/configs/nvimtree.lua
+++ lua/user/plugins/configs/nvimtree.lua
@@ -1,5 +1,6 @@
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.git.ignore = false
+lvim.builtin.nvimtree.setup.open_on_setup = true
 lvim.builtin.nvimtree.setup.actions.open_file.resize_window = true
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
 lvim.builtin.nvimtree.setup.renderer.icons.glyphs.git = {
